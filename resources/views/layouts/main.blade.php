abdcd7f2ba69739caec593a22f86500a64a8180f
marcotrunc <marcotrunc@gmail.com>
2022-03-22 15:58:07 +0100

    created new views

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-@extends('layouts.main')
+    @include('includes.header')
+
+    {{-- Main --}}
+    @yield('content-main')
 </body>
 </html>
