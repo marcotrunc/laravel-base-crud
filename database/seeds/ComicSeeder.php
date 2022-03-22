a1cc7159ec1cb145710dd4b1b28690fa9a90cfc7
marcotrunc <marcotrunc@gmail.com>
2022-03-22 15:17:36 +0100

    created and populated db

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Moldels\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+        foreach ($comics as $comic) {
+            $new_comic = new Comic();
+            $new_comic->fill($comic);
+            $new_comic->save();
+        }
+    }
+}
